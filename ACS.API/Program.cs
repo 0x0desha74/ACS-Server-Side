aa086b52ea88b68692c62463c3f169df2fc1dc61
0x0desha74 <mudesha74@gmail.com>
2025-06-25 05:44:24 +0300

    Set Up IdentityDB

--- ACS.API/Program.cs
+++ ACS.API/Program.cs
@@ -1,4 +1,7 @@
 
+using ACS.Repository.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace ACS.API
 {
     public class Program
@@ -13,6 +16,12 @@ namespace ACS.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                var identityConnectionString = builder.Configuration.GetConnectionString("IdentityConnection");
+                options.UseSqlServer(identityConnectionString);
+            });
 
             var app = builder.Build();
 
---
+++ ACS.Repository/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACS.Repository.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext
+    {
+        public AppIdentityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppIdentityDbContext()
+        {
+        }
+    }
+}
